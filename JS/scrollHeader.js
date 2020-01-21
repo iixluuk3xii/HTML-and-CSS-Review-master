be3902f208615025182e3b5e5ffd9992f4fa0abc
luke.youngman <luke.youngman@NM-19838-PC>
2020-01-21 16:08:18 +0000

    attemted a few bug fixes for the scroll header

--- JS/scrollHeader.js
+++ JS/scrollHeader.js
@@ -4,53 +4,48 @@ const header = document.querySelector('.header');
 let position = document.documentElement.scrollTop;
 
 const fixToWindow = (element) => {
-    if (window.scrollY > 257) {
+    if (window.scrollY > 260) {
         element.classList.add("fix");
-        overflow.style.height = "256.56px";
-
-    } else if (window.scrollY <= 257) {
+        element.style.height = "256.56px";
+    } else {
         element.classList.remove("fix");
-        overflow.style.height = "";
-        
+        element.style.height = "";
+      
+
     }
 }
 
 const toggleTopMargin = (element) => {
-    if (window.scrollY > 257) {
+    if (window.scrollY > 260) {
         element.classList.add("margin");
     } else {
         element.classList.remove("margin");
     }
-
-}
-
-const pushElementUp = (element) => {
-    if (window.scrollY > 257) {
-        element.classList.add("top");
-        element.style.transform = "translateY(-211.59px)"
-    } else if (window.scrollY > 0) {
-        element.style.transform = "translateY(0)"
-    }
 }
 
 document.addEventListener('scroll', () => {
-    
+
     var scroll = document.documentElement.scrollTop;
-        
+
     fixToWindow(overflow);
     toggleTopMargin(gallery);
-    pushElementUp(header);
 
     if (scroll > position && window.scrollY > 256) {
         console.log('down');
-        header.style.transform = "translateY(-211.59px)";
-        header.style.transition = "transform 0.25s";
-      } else {
+        setTimeout(() => {
+            header.style.transform = "translateY(-211.59px)";
+            header.style.transition = "transform 0.25s";
+        }, 200);
+    } else {
         console.log('up');
-        header.style.transform = "translateY(0px)";
-        header.style.transition = "transform 0.25s";
+        setTimeout(() => {
+            header.style.transform = "translateY(0px)";
+            header.style.transition = "transform 0.25s";
+        }, 200);
         
-      }
+    }
+
+
 
     position = scroll;
 
