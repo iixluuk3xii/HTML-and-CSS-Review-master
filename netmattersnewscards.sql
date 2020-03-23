d975ed9b5098267d9141c2ab083c91b02913b4f3
iixluuk3xii <36408448+iixluuk3xii@users.noreply.github.com>
2020-03-23 11:59:38 +0000

    updated news cards DB

--- netmattersnewscards.sql
+++ netmattersnewscards.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 23, 2020 at 10:30 AM
+-- Generation Time: Mar 23, 2020 at 12:59 PM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.3
 
@@ -21,8 +21,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `netmattersnewscards`
 --
-CREATE DATABASE IF NOT EXISTS `netmattersnewscards` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-USE `netmattersnewscards`;
 
 -- --------------------------------------------------------
 
@@ -39,17 +37,38 @@ CREATE TABLE `newscards` (
   `color` varchar(255) CHARACTER SET utf8 NOT NULL,
   `authorIcon` varchar(255) CHARACTER SET utf8 NOT NULL,
   `authorName` varchar(255) CHARACTER SET utf8 NOT NULL,
-  `datePosted` date NOT NULL
+  `datePosted` date NOT NULL,
+  `category` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `newscards`
 --
 
-INSERT INTO `newscards` (`cardID`, `IMG`, `toolTip`, `title`, `desc`, `color`, `authorIcon`, `authorName`, `datePosted`) VALUES
-(1, '../IMGs/full-stack-web-l5m7.jpg', 'Web Design / Careers', 'Full stack developer', 'Salary: £25,000 - £36,000 per annum + Bonus (Dependent on Experience) Hours: 40 hours per week, M...', '#926fb1', '../IMGs/josh-ralph-icon.jpg', 'Josh Ralph', '2019-11-22'),
-(2, '../IMGs/2nd-line-support.jpg', 'IT Support / Careers', '2nd Line support technician', 'SALARY: £22,000 - £28,000 + Bonus & Pension Netmatters is a rapidly growing IT and website de...', '#286abd', '../IMGs/rob-george-icon.jpg', 'Rob George', '2019-11-21'),
-(3, '../IMGs/imagine-connecting-to.jpg', 'Telecoms Services / Insights', 'Imagine: Connecting to Your Business (and Cus...', 'Technology accelerates at a dizzying pace. And technology feels like it\'s accelerating because i...', '#c42e2a', '../IMGs/evan-thomas-icon.jpg', 'Evan Thomas', '2019-11-18');
+INSERT INTO `newscards` (`cardID`, `IMG`, `toolTip`, `title`, `desc`, `color`, `authorIcon`, `authorName`, `datePosted`, `category`) VALUES
+(1, '../IMGs/full-stack-web-l5m7.jpg', 'Web Design / Careers', 'Full stack developer', 'Salary: £25,000 - £36,000 per annum + Bonus (Dependent on Experience) Hours: 40 hours per week, M...', '#926fb1', '../IMGs/josh-ralph-icon.jpg', 'Josh Ralph', '2019-11-22', 'CAREERS'),
+(2, '../IMGs/2nd-line-support.jpg', 'IT Support / Careers', '2nd Line support technician', 'SALARY: £22,000 - £28,000 + Bonus & Pension Netmatters is a rapidly growing IT and website de...', '#286abd', '../IMGs/rob-george-icon.jpg', 'Rob George', '2019-11-21', 'CAREERS'),
+(3, '../IMGs/imagine-connecting-to.jpg', 'Telecoms Services / Insights', 'Imagine: Connecting to Your Business (and Cus...', 'Technology accelerates at a dizzying pace. And technology feels like it\'s accelerating because i...', '#c42e2a', '../IMGs/evan-thomas-icon.jpg', 'Evan Thomas', '2019-11-18', 'INSIGHTS');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `newscards`
+--
+ALTER TABLE `newscards`
+  ADD PRIMARY KEY (`cardID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `newscards`
+--
+ALTER TABLE `newscards`
+  MODIFY `cardID` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
