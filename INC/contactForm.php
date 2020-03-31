62877492738ec110d16c769947d7972c5df810c1
iixluuk3xii <36408448+iixluuk3xii@users.noreply.github.com>
2020-03-31 12:18:36 +0100

    Added responsive code

--- INC/contactForm.php
+++ INC/contactForm.php
@@ -10,7 +10,9 @@
     </ul>
 </div>
 <div class="title">
-    <h1>How Can We Help You?</h1>
+    <div class="fixed-width">
+        <h1>How Can We Help You?</h1>
+    </div>
 </div>
 <div class="form-background">
     <div id="form">
@@ -35,21 +37,21 @@
             </div>
             <form method="POST" accept-charset="UTF-8" id="contact-form" action="contactAction.php" novalidate="novalidate">
                 <div class="row">
-                    <span class="field">
+                    <span class="field name">
                         <label for="name">Your Name </label>
                         <input name="name" autocomplete="on" type="text" id="name" value="<?php if (isset($_POST["name"])) echo $_POST["name"]; ?>">
                     </span>
-                    <span class="field">
+                    <span class="field email">
                         <label for="email">Your Email </label>
                         <input name="email" autocomplete="on" type="text" id="email" value="<?php if (isset($_POST["email"])) echo $_POST["email"]; ?>">
                     </span>
                 </div>
                 <div class="row">
-                    <span class="field">
+                    <span class="field number">
                         <label for="number">Your Telephone Number </label>
                         <input name="number" autocomplete="on" type="tel" id="number" value="<?php if (isset($_POST["number"])) echo $_POST["number"]; ?>">
                     </span>
-                    <span class="field">
+                    <span class="field subject">
                         <label for="subject">Subject </label>
                         <input name="subject" type="text" id="subject" value="<?php if (isset($_POST["subject"])) echo $_POST["subject"]; ?>">
                     </span>
@@ -110,7 +112,7 @@
 
     <div class="container">
 
-        <div id="cards" class="cards location">
+        <div id="cards" class="cards location map-padding">
 
             <div class="map">
                 <div class="map--card">
@@ -134,7 +136,7 @@
                     </a>
 
                 </div>
-                <div>
+                <div class="map-holder">
                     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2424.650833026772!2d1.1340782156411313!3d52.57592537982482!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47d9ddac8dba0b4b%3A0x9c77ffbfe7911dab!2sNetmatters!5e0!3m2!1sen!2suk!4v1584707013706!5m2!1sen!2suk" width="555" height="300" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
                 </div>
             </div>
@@ -161,7 +163,7 @@
                     </a>
 
                 </div>
-                <div>
+                <div class="map-holder">
                     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2425.7234908590995!2d1.7104906156403177!3d52.55652637982112!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47da0593b311cec3%3A0x1cb3c1d4c0b340f6!2sNetmatters%20Gorleston!5e0!3m2!1sen!2suk!4v1584707519744!5m2!1sen!2suk" width="555" height="300" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
                 </div>
             </div>
